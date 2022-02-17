1f2b20f25ae657868e5cd6c370a25f1d52f71608
Hubert Kucharski <99823278+Hubert-Kucharski@users.noreply.github.com>
2022-02-17 10:59:10 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+volatile unsigned char send_enable = 0;
+void UART_Init(void)
+{
+	UBRRH = (51>>8);
+	UBRRL = 51;										// baud rate 9600bps
+	UCSRB |= (1<<TXEN);								// transmitter enable
+	UCSRC |=  (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0);		// 8 bit mode
+	
+}
+
+void UART_Transmit(unsigned char byte)
+{
+	while ( !( UCSRA & (1<<UDRE)) )		// wait for empty transmit buffer
+	;
+	UDR = byte;
+}
+
+void Button_Init(void)
+{
+	MCUCR |= (1<<ISC11);				// the falling edge of INT1 generates an interrupt request
+	GICR |= (1<<INT1);
+}
+
+ISR(INT1_vect)
+{
+	_delay_ms(5);						// Simple debouncer
+	if (!(PIND&(1<<PD3)))
+	send_enable = 1;
+
+}
+int main(void)
+{
+    UART_Init();
+	Button_Init();
+	sei();							// global interrupt flag set
+	unsigned char counter = 0;		// data to be send
+	DDRB |= (1<<PB0);				// LED indicator port
+    while (1) 
+    {
+				PORTB &= (~(1<<PB0));	// LED off
+				if (send_enable)
+				{
+					PORTB |= (1<<PB0);	// LED on
+					send_enable = 0;
+					counter++;
+					UART_Transmit(counter);
+				}
+    }
+}
+
---
+++ uart_fpga.vhd
@@ -0,0 +1,88 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity uart_fpga is
+port(
+	Rx: in std_logic;
+	CLK: in std_logic;
+	LED: out std_logic_vector(7 downto 0)
+	);
+end uart_fpga;
+
+architecture Behavioral of uart_fpga is
+type states is (IDLE,START,RUN,STOP);
+signal state: states := IDLE;
+signal next_state: states := IDLE;
+signal Rx_data: std_logic_vector(7 downto 0) := (others => '0');
+
+begin
+		
+data: process(CLK)
+variable bit_number: integer range 0 to 8;
+variable counter: integer range 0 to 16000 := 0;
+begin
+
+	if rising_edge(CLK) then
+		case state is
+			when IDLE =>
+				bit_number := 0;
+				counter := 0;
+				if Rx = '0' then				-- When other device starts transmission
+					state <= START;			
+				else
+					state <= IDLE;
+				end if;
+				
+			when START =>
+				if counter = 15600 then 			-- 1,5 bit delay
+					Rx_data(bit_number) <= Rx;
+					bit_number := 1;
+					counter := 0;
+					state <= RUN;				
+				else
+					counter := counter + 1;
+					state <= START;
+				end if;			
+			
+			when RUN =>
+				if counter = 10400 then 			-- 1 bit delay
+					Rx_data(bit_number) <= Rx;		-- Bits assign
+					bit_number := bit_number + 1;
+					counter := 0;
+					state <= RUN;
+				else
+					if bit_number = 8 then		-- After 1 byte has been received
+						counter := 0;
+						bit_number := 0;
+						state <= STOP;
+					else
+						counter := counter + 1;
+						state <= RUN;
+					end if;
+				end if;
+			when STOP =>
+				bit_number := 0;
+				if counter = 10400 then
+					counter := 0;
+					state <= IDLE;
+				else
+					counter := counter + 1;
+					state <= STOP;
+				end if;
+		end case;
+	end if;
+end process;
+
+
+LED_output_change: process(CLK,state)					-- Assign received data to LEDs
+begin
+	if rising_edge(CLK) and state = IDLE then
+		LED <= Rx_data;
+	elsif rising_edge(CLK) and (state = START or state = RUN or state = STOP) then
+		LED <= "00000000";
+	end if;		
+end process;
+
+end Behavioral;
+
